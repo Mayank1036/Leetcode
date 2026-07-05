884b39d1302891e3f5251f369a561e9b0858adee
Mayank Saini <mayanksaini3006@gmail.com>
2026-07-05 09:40:28 +0530

    Time: 0 ms (100%), Space: 42.9 MB (35.27%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,12 +10,38 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ArrayList<ListNode> arr = new ArrayList<>();
-        ListNode temp = head;
-        while( temp != null){
-            arr.add(temp);
-            temp = temp.next;
+        ListNode slow = head; // most optinal and easy tc , sc = O(n)
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return arr.get(arr.size() / 2);
-    }
+        return slow;
+    }  
 }
+
+
+        // int len = 0; // finding length of list tc = O(n) and sc = O(1);
+        // ListNode temp = head;
+        // while(temp != null){
+        //     len++;
+        //     temp = temp.next;
+        // }
+
+        // len = len/2;
+        // temp = head;
+        // while(len != 0){
+        //     temp = temp.next;
+        //     len--;
+        // }
+        // return temp;
+
+    //     ArrayList<ListNode> arr = new ArrayList<>();  // takes extra space to store O(n) converting to arr
+    //     ListNode temp = head;
+    //     while( temp != null){
+    //         arr.add(temp);
+    //         temp = temp.next;
+    //     }
+    //     return arr.get(arr.size() / 2);
+    // }
