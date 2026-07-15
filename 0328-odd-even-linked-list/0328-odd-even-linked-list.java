13a1d2d30b07e4a7f9c91eb8b1c567e73afcc640
Mayank Saini <mayanksaini3006@gmail.com>
2026-07-15 22:45:05 +0530

    Time: 0 ms (100%), Space: 46.4 MB (55.54%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -11,28 +11,41 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head == null || head.next == null) return head;
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null && temp.next != null){ // for odd elements
-            list.add(temp.val);
-            temp = temp.next.next;
-        }
-        if(temp != null) list.add(temp.val);
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next; // we can also take evenHead = even
+        while( even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
 
-        temp = head.next;
-        while(temp != null && temp.next != null){ // for even elements
-            list.add(temp.val);
-            temp = temp.next.next;
+            odd = odd.next;
+            even = even.next;
         }
-        if(temp != null) list.add(temp.val);
-
-        int i=0 ;
-        temp = head;
-        while(temp != null){
-            temp.val = list.get(i);
-            i++;
-            temp = temp.next;
-        }
+        odd.next = evenHead;
         return head;
     }
 }
+        // if(head == null || head.next == null) return head;
+        // ArrayList<Integer> list = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp != null && temp.next != null){ // for odd elements
+        //     list.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null) list.add(temp.val);
+
+        // temp = head.next;
+        // while(temp != null && temp.next != null){ // for even elements
+        //     list.add(temp.val);
+        //     temp = temp.next.next;
+        // }
+        // if(temp != null) list.add(temp.val);
+
+        // int i=0 ;
+        // temp = head;
+        // while(temp != null){
+        //     temp.val = list.get(i);
+        //     i++;
+        //     temp = temp.next;
+        // }
+        // return head;
